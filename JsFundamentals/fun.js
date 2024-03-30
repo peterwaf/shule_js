bb531e5990322464214acf92e4090b7b6688e6ee
peterwaf <peterwafulah@gmail.com>
2024-03-30 11:57:58 +0300

    js

---
+++ JsFundamentals/fun.js
@@ -0,0 +1,41 @@
+// Varaibles 
+// 1.What is the output of the following code in the console when the operands are added using arithmetic operator?
+// let x = 5;
+// let y = 10;
+// let z = 15;
+
+// 2.Create a variable called carName, assign the value Volvo to it.
+
+// 3.Create a variable called z, assign x + y to it, and display the result in an   
+//   console Tab.
+
+// Operators:
+// 4. Multiply 10 with 5, and console the result
+
+// 5. Alert the remainder when 15 is divided by 9.
+
+// Functions:
+
+// 6. Create a function called "myFunction" and give a block of code so that when it    
+//    is called it executes an alert with the value assigned in your code.
+
+// 7.Make a function that returns "Hello" and display the value in the console tab.
+
+
+
+// Arrays: 
+// 8. Show your result in the console of the browser.
+//  a) Create an array of fruits and:
+//     i) Use the correct array method to remove the last item in the array.
+       
+//    ii) Use the correct Array method to add a fruit at the begining of the fruits 
+//        array
+
+// Loops:
+// 9. Display your outputs in the console:
+//    a) Create a loop that runs from 0 to 9 using the for loop.
+//    b) Create a loop that runs through each item in the below array:
+//       const fruits = ["Apple", "Banana", "Orange"];
+
+// Data Types:
+
